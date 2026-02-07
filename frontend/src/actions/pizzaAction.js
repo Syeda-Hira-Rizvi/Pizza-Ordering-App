5253bfb7ce6fa70e2e4938e737fca04d3fb85e5e
Syeda Hira Rizvi <rizvisyedahira@gmail.com>
2026-02-07 21:00:09 +0500

    changing process.env according to vite in all api calls

--- frontend/src/actions/pizzaAction.js
+++ frontend/src/actions/pizzaAction.js
@@ -3,7 +3,7 @@ import swal from "sweetalert";
 export const getAllPizzas = () => async (dispatch) => {
   dispatch({ type: "GET_PIZZAS_REQUEST" });
   try {
-    const response = await axios.get(`${process.env.VITE_APP_BASE_URL}/pizzas/getAllPizzas`);
+    const response = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/pizzas/getAllPizzas`);
     // console.log(response.data);
     dispatch({ type: "GET_PIZZAS_SUCCESS", payload: response.data });
   } catch (err) {
@@ -14,7 +14,7 @@ export const getAllPizzas = () => async (dispatch) => {
 export const addPizza = (pizza) => async (dispatch) => {
   dispatch({ type: "ADD_PIZZAS_REQUEST" });
   try {
-    await axios.post(`${process.env.VITE_APP_BASE_URL}/pizzas/addpizza`, { pizza });
+    await axios.post(`${import.meta.env.VITE_APP_BASE_URL}/pizzas/addpizza`, { pizza });
     dispatch({ type: "ADD_PIZZAS_SUCCESS" });
   } catch (err) {
     dispatch({ type: "ADD_PIZZAS_FAIL", payload: err });
@@ -24,7 +24,7 @@ export const addPizza = (pizza) => async (dispatch) => {
 export const getPizzaById = (pizzaId) => async (dispatch) => {
   dispatch({ type: "GET_PIZZABYID_REQUEST" });
   try {
-    const response = await axios.post(`${process.env.VITE_APP_BASE_URL}/pizzas/getpizzabyid`, { pizzaId });
+    const response = await axios.post(`${import.meta.env.VITE_APP_BASE_URL}/pizzas/getpizzabyid`, { pizzaId });
     dispatch({ type: "GET_PIZZABYID_SUCCESS", payload: response.data });
   } catch (err) {
     dispatch({ type: "GET_PIZZABYID_FAIL", payload: err });
@@ -33,7 +33,7 @@ export const getPizzaById = (pizzaId) => async (dispatch) => {
 export const updatePizza = (updatedPizza) => async (dispatch) => {
   dispatch({ type: "UPDATE_PIZZABYID_REQUEST" });
   try {
-    const response = await axios.post(`${process.env.VITE_APP_BASE_URL}/pizzas/updatepizza`, {
+    const response = await axios.post(`${import.meta.env.VITE_APP_BASE_URL}/pizzas/updatepizza`, {
       updatedPizza,
     });
     dispatch({ type: "UPDATE_PIZZABYID_SUCCESS", payload: response.data });
@@ -45,12 +45,12 @@ export const updatePizza = (updatedPizza) => async (dispatch) => {
 
 export const deletePizza = (pizzaId) => async (dispatch) => {
   try {
-    await axios.post(`${process.env.VITE_APP_BASE_URL}/pizzas/deletepizza`, { pizzaId });
-    swal("Pizza Deleted Succss!", "success");
+    await axios.post(`${import.meta.env.VITE_APP_BASE_URL}/pizzas/deletepizza`, { pizzaId });
+    swal("Pizza Deleted Success!", "success");
     window.location.href = "/admin/pizzalist";
     // console.log(res);
   } catch (error) {
-    swal("Errro While Deleteing Pizza");
+    swal("Error While Deleteing Pizza");
   }
 };
 
@@ -58,7 +58,7 @@ export const filterPizza = (searchkey, category) => async (dispatch) => {
   let filterdPizza;
   dispatch({ type: "GET_PIZZAS_REQUEST" });
   try {
-    const res = await axios.get(`${process.env.VITE_APP_BASE_URL}/pizzas/getAllPizzas`);
+    const res = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/pizzas/getAllPizzas`);
     filterdPizza = res.data.filter((pizza) =>
       pizza.name.toLowerCase().includes(searchkey)
     );
